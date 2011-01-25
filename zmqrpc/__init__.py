d52f91c872891c85b7ae56a31ade6fa2e3399000
Geoff Watts <geoff@editd.com>
2011-01-25 13:28:50 +0000

    prettier printing tracebacks

--- zmqrpc/__init__.py
+++ zmqrpc/__init__.py
@@ -1,18 +1,22 @@
 """Provides an RPC interface for a class via zmq and BSON""" 
-__version__ = "0.1b"
+__version__ = "0.1c"
 __author__ = "Geoff Watts"
 
 class ZMQRPCError(Exception):
     def __init__(self, value):
      self.value = value
     def __str__(self):
-     return repr(self.value)
+     return str(self.value)
+    def __unicode__(self):
+     return unicode(self.value)
 
 class ZMQRPCRemoteError(Exception):
     def __init__(self, value):
       self.value = value
     def __str__(self):
-      return repr(self.value)
+      return str(self.value)
+    def __unicode__(self):
+     return unicode(self.value)
       
       
 
